bd3e9d24f31886ac356b1514582b68337000e703
em-2233 <epmelimartinez@gmail.com>
2024-11-05 00:21:28 -0600

    adding project files

---
+++ scripts/character_body_2d.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+@export var speed := 75.0
+@export var acceleration := 1000.0  # How quickly the character reaches full speed
+
+# Velocity that will gradually increase/decrease
+var current_velocity := Vector2.ZERO
+
+func _process(delta: float) -> void:
+	# Get the input direction vector directly
+	var input_direction = Vector2(
+		int(Input.is_action_pressed("ui_right"))
+		 - int(Input.is_action_pressed("ui_left")),
+		int(Input.is_action_pressed("ui_down"))
+		 - int(Input.is_action_pressed("ui_up"))
+	)
+
+	input_direction = input_direction.normalized()
+	current_velocity = input_direction * speed
+
+
+	# Apply the smoothed velocity
+	self.velocity = current_velocity
+	move_and_slide()
+
+## Could be used for slippery surfaces
+	## Normalize if there's any input
+	#if input_direction.length() > 0:
+		#input_direction = input_direction.normalized()
+		## No acceleration period
+		#current_velocity = input_direction * speed
+#
+		## Smooth acceleration period
+		##current_velocity = current_velocity.move_toward(
+			##input_direction * speed, acceleration * delta)
+	#else:
+		## No deceleration period
+		#current_velocity = input_direction * speed
+		#
+		## Smooth deceleration period
+		##current_velocity = current_velocity.move_toward(
+			##Vector2.ZERO, acceleration * delta)
